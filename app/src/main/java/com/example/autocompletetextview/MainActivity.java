0e80db0a71dcb45a9503808e6fcf945e69226ffd
Ramiz Khan <ramizkhan431@gmail.com>
2019-07-10 12:08:53 +0530

    just test

--- app/src/main/java/com/example/autocompletetextview/MainActivity.java
+++ app/src/main/java/com/example/autocompletetextview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.autocompletetextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTextView;
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter=new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,values);
         autoCompleteTextView.setAdapter(adapter);
         autoCompleteTextView.setThreshold(1);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Working fine..", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
